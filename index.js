7ec6da0763dd44fb70189fae8ac7c300322ad9eb
Sdavis <sdavis@Stevens-MacBook-Pro.local>
2019-05-14 18:01:58 -0600

    Initial Commit

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+require("dotenv").config();
 const express = require("express");
+const massive = require("massive");
+const controller = require("./controller");
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance);
+
+  // dbInstance.new_planes()
+  //   .then( planes => console.log( planes ) )
+  //   .catch( err => console.log( err ) );
+
+  // dbInstance.get_planes()
+  //   .then(planes => console.log(planes))
+  //   .catch(err => console.log(err));
+});
 
 app.use(express.json());
+
+app.get("/api/planes", controller.getPlanes);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
